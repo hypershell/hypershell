05f57b76f7c2095301aeee67a72f14248033920d
Geoffrey Lentner <glentner@purdue.edu>
2026-07-05 04:25:57 -0400

    [release] Update copyright year to 2026

--- src/hypershell/cluster/__init__.py
+++ src/hypershell/cluster/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 Geoffrey Lentner
+# SPDX-FileCopyrightText: 2026 Geoffrey Lentner
 # SPDX-License-Identifier: Apache-2.0
 
 """Run cluster with server and managed clients."""
